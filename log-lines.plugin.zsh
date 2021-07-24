e3761e65de0f7fe67a0bcb53e4c6814c1d393340
Adam Hitchcock <adam@northisup.com>
2021-07-23 20:32:57 -0400

    initial commit

---
+++ log-lines.plugin.zsh
@@ -0,0 +1,9 @@
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+# Then ${0:h} to get plugin’s directory
+
+fpath+=( "${0:h}/functions" )
+path+=( "${0:h}/bin" )
+
+autoload functions/*
